2474d60ff4e43109368ae353bdb8439fa524b1da
Steven J.S Min <stevenmin@nbnco.com.au>
2019-05-26 15:58:10 +1000

    Add tags info

--- packer/provisioning.scripts/install-cicd-tools.sh
+++ packer/provisioning.scripts/install-cicd-tools.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
-# TODO Scripting
-
-echo "Hello from install-cicd-tools.sh" > /home/ec2-user/provisioning.scripts/install-cicd-tools.txt
+# Download CICD Tools
+"sudo curl -o /opt/cicd-tools-deploymentManager-20190521.tar.gz http://stevenmin.info/cicd-tools/cicd-tools-deploymentManager-20190521.tar.gz"
+"sudo curl -o /opt/cicd-tools-fitnesse-20190521.tar.gz http://stevenmin.info/cicd-tools/cicd-tools-fitnesse-20190521.tar.gz"
+"sudo curl -o /opt/cicd-tools-java-20190521.tar.gz http://stevenmin.info/cicd-tools/cicd-tools-java-20190521.tar.gz"
+"sudo curl -o /opt/cicd-tools-jenkins-20190521.tar.gz http://stevenmin.info/cicd-tools/cicd-tools-jenkins-20190521.tar.gz"
