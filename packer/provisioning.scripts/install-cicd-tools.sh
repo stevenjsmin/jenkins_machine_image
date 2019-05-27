1bbbed85883fcc415982f79ed9a136b946845618
Steven J.S Min <stevenmin@nbnco.com.au>
2019-05-27 15:54:12 +1000

    Steven change

--- packer/provisioning.scripts/install-cicd-tools.sh
+++ packer/provisioning.scripts/install-cicd-tools.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-echo "Hello from install-cicd-tools.sh" > /home/ec2-user/provisioning.scripts/install-cicd-tools.txt
-
-# Download CICD Tools
+# Install CICD Tools
 sudo tar xvfz /home/ec2-user/cicd-tools/cicd-tools-deploymentManager-20190527.tar.gz -C /opt/
 sudo tar xvfz /home/ec2-user/cicd-tools/cicd-tools-fitnesse-20190527.tar.gz -C /opt/
 sudo tar xvfz /home/ec2-user/cicd-tools/cicd-tools-java-20190527.tar.gz -C /opt/
 sudo tar xvfz /home/ec2-user/cicd-tools/cicd-tools-jenkins-20190527.tar.gz -C /opt/
-# sudo tar xvfz /home/ec2-user/cicd-tools/cicd-tools-wars-20190527.tar.gz -C /opt/
+
+# Install Web-Application for Appian CICD Portal
+sudo tar xvfz /home/ec2-user/cicd-tools/cicd-tools-wars-20190527.tar.gz -C /home/ec2-user
