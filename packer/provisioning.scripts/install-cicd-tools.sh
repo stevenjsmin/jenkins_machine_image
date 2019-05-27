7d869134026dc4585e9409ec0d6a43af802f8319
Steven J.S Min <stevenmin@nbnco.com.au>
2019-05-27 10:58:36 +1000

    Add tags info

--- packer/provisioning.scripts/install-cicd-tools.sh
+++ packer/provisioning.scripts/install-cicd-tools.sh
@@ -3,7 +3,7 @@
 echo "Hello from install-cicd-tools.sh" > /home/ec2-user/provisioning.scripts/install-cicd-tools.txt
 
 # Download CICD Tools
-sudo tar xvfz cicd-tools-deploymentManager-20190521.tar.gz -C /opt/
-sudo tar xvfz cicd-tools-fitnesse-20190521.tar.gz -C /opt/
-sudo tar xvfz cicd-tools-java-20190521.tar.gz -C /opt/
-sudo tar xvfz cicd-tools-jenkins-20190521.tar.gz -C /opt/
+sudo tar xvfz /home/ec2-user/cicd-tools/cicd-tools-deploymentManager-20190521.tar.gz -C /opt/
+sudo tar xvfz /home/ec2-user/cicd-tools/cicd-tools-fitnesse-20190521.tar.gz -C /opt/
+sudo tar xvfz /home/ec2-user/cicd-tools/cicd-tools-java-20190521.tar.gz -C /opt/
+sudo tar xvfz /home/ec2-user/cicd-tools/cicd-tools-jenkins-20190521.tar.gz -C /opt/
