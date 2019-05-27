1ea36e3c44518ca4d550a7f9b258e52ce241c777
Steven J.S Min <stevenmin@nbnco.com.au>
2019-05-27 16:22:44 +1000

    Steven change

--- packer/provisioning.scripts/setup-evironment.sh
+++ packer/provisioning.scripts/setup-evironment.sh
@@ -5,3 +5,7 @@ sudo cp /home/ec2-user/cicd-tools/appianci.sh /etc/profile.d/
 
 # Set /etc/hosts
 sudo echo "198.18.2.228    yum.nbnnal.local" >> /etc/hosts
+
+
+#Set Database URL
+# sed -i "/DB.URL=/ s/=.*/=${DB_URL}/" "/home/jenkins/appiancicd.sys.properties"
